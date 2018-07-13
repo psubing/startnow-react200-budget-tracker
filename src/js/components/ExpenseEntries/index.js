8f8aa38a50ef7ca31f1a846293ba2b34de0765e6
psubing <psubing@gmail.com>
2018-07-13 13:04:43 -0700

    adds initial project files

---
+++ src/js/components/ExpenseEntries/index.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import ExpenseEntries from './ExpenseEntries';
+
+function mapStoreToProps(store) {
+  return {
+    description: store.expense.description,
+    amount: store.expense.amount,
+    lineItems: store.expense.lineItems
+  };
+}
+
+export default connect(mapStoreToProps)(ExpenseEntries);
